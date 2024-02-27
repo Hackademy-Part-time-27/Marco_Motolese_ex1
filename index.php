d5bb531f0111e4fc837e53ff271a8088ae50eb5b
Marco Motolese <marcomotolese777@gmail.com>
2024-02-27 22:36:03 +0100

    pushEsercitazione

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+$integer = 10;
+
+$float = 1.2;
+
+$string = "hello";
+
+$boolean = true;
+
+
+const INTEGER = 15;
+
+
+
+echo "il valore di integer è ; "  . INTEGER . "\n" ;
+
+echo "Tipo di dato di integer: " . gettype($integer) . "\n";
+
+echo "Tipo di dato di float: " . gettype($float) . "\n";
+
+echo "Tipo di dato di string: " . gettype($string) . "\n";
+
+echo "Tipo di dato di boolean: " . gettype($boolean) . "\n";
+
+
+
+
+
+const FLOAT = 1.5;
+
+
+
+echo "il valore di float  è ; "  . FLOAT . "\n" ;
+
+
+
+
+const STRING = "giovanni";
+
+
+
+echo "il valore di string  è ; "  . STRING . "\n" ;
+
+
+
+
+
+
+
+
+
+const INTEGER = 15;
+
+
+
+echo "il valore di integer è ; "  . INTEGER . "\n" ;
+
+
+
+
+
+//traccia 2
+
+
+
+
+$text1 = "Marco";     //non si crea una variabile con un numero iniziale
+$text2 = "hai";
+$text3 = "sete"; //non si può utilizzare il punto nel dichiarare una variabile
+$text4 = "?";
+$text5 = "Perchè"; //non si crea una variabile con il simbolo della chiocciola
+$text6 = $text2; // il trattino non è consentito  nei nomi della variabile , gli apici vanno tolti 
+$text7 = 'bevuto';   // non è consentito lo spazio nelle variabili
+$text8 = "tutto" ;  // manca il ; finale
+
+
+
+
+echo $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+
+
+
+
+
+
+
+
+
+
+
